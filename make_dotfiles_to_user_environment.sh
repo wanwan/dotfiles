4cabe29fe4543903bb1688cb8ded1a897f748d31
WAKABAYASHI Ayumu <waka@zaregoto.org>
2016-10-01 10:48:58 +0900

    copy scripts from howm/tools to private

--- make_dotfiles_to_user_environment.sh
+++ make_dotfiles_to_user_environment.sh
@@ -2,3 +2,7 @@
 
 git clone https://github.com/wanwan/dotfiles.private.git private
 ln -s $HOME/dotfiles/emacs.d $HOME/.emacs.d
+
+if [ -d private -a -x private/make_dotfiles_to_user_environment.sh ]; then
+	private/make_dotfiles_to_user_environment.sh
+fi
