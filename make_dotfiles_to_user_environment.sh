47359f566fedcef0d24cf41aadf43fb6df4e4da0
WAKABAYASHI, Ayumu <sakuraduka@gmail.com>
2017-06-21 18:27:20 +0900

    modify xmonad.hs for dzen2 fullscreen setting

--- make_dotfiles_to_user_environment.sh
+++ make_dotfiles_to_user_environment.sh
@@ -5,6 +5,7 @@ ln -s $HOME/dotfiles/emacs.d $HOME/.emacs.d
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/bash_profile $HOME/.bash_profile
 ln -s $HOME/dotfiles/screenrc $HOME/.screenrc
+ln -s $HOME/dotfiles/xmonad $HOME/.xmonad
 
 if [ -d private -a -x private/make_dotfiles_to_user_environment.sh ]; then
 	private/make_dotfiles_to_user_environment.sh
