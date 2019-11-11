3d3e86df32ad763ff76093728d3a47b292b0bf06
WAKABAYASHI, Ayumu <sakuraduka@gmail.com>
2019-11-11 10:20:51 +0900

    add cask package

---
+++ emacs.d/elpa/ansi-20150703.826/ansi-autoloads.el
@@ -0,0 +1,15 @@
+;;; ansi-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ansi.el") (24003 43534 317522 740000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ansi-autoloads.el ends here
