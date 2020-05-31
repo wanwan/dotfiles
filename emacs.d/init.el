5a5b2e78b3b3e1d16954a937faa9afce475092bc
WAKABAYASHI, Ayumu <Ayumu.Wakabayashi@sony.com>
2020-06-01 01:19:27 +0900

    add flycheck / irony settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -345,6 +345,40 @@
   (define-key company-active-map (kbd "C-h") nil) ;; C-hはバックスペース割当のため無効化
   )
 
+;; irony
+(require 'irony)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-to-list 'company-backends 'company-irony) ; backend追加
+
+;; flycheck
+(global-flycheck-mode)
+
+
+;; 連想リストの中身を文字列のリストに変更せず、変数そのままの構造を利用。
+;; 複数のコンパイルオプションはスペースで区切る
+(setq irony-lang-compile-option-alist
+      (quote ((c++-mode . "c++ -std=c++11 -lstdc++")
+              (c-mode . "c")
+              (objc-mode . "objective-c"))))
+;; アドバイスによって関数を再定義。
+;; split-string によって文字列をスペース区切りでリストに変換
+;; (24.4以降 新アドバイス使用)
+(defun ad-irony--lang-compile-option ()
+  (defvar irony-lang-compile-option-alist)
+  (let ((it (cdr-safe (assq major-mode irony-lang-compile-option-alist))))
+    (when it (append '("-x") (split-string it "\s")))))
+(advice-add 'irony--lang-compile-option :override #'ad-irony--lang-compile-option)
+;; (24.3以前 旧アドバイス使用)
+(defadvice irony--lang-compile-option (around ad-irony--lang-compile-option activate)
+  (defvar irony-lang-compile-option-alist)
+  (let ((it (cdr-safe (assq major-mode irony-lang-compile-option-alist))))
+    (when it (append '("-x") (split-string it "\s")))))
+
+
+
 ;; rtags
 ;; only run this if rtags is installed
 (when (require 'rtags nil :noerror)
@@ -375,7 +409,8 @@
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode)
-              (rtags-mode))
+              ;;(rtags-mode)
+	      )
             (when (derived-mode-p 'java-mode)
               (ggtags-mode 1))))
 ;; Enable helm-gtags-mode
