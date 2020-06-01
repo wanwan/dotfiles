537e090729bdd8d5350ad46337043b6e4f7e7f7c
WAKABAYASHI,Ayumu <xxxxx>
2020-06-01 21:10:19 +0900

    temporary commit to separate private dir

--- script/sony.habb/build_hagodaemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORKDIR=/home/waka/workspace
-EBISUDIR=${WORKDIR}/hgrm/master/ebisu-bsp
-RELEASE=debug
-
-cd ${EBISUDIR}
-
--- script/sony.habb/make_repo_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x 
-
-export MIRROR=~/workspace/hgrm/mirror
-mkdir -p $MIRROR
-cd $MIRROR
-repo init -u ssh://github.com/route384/hgrm.bsp.manifest -b pf/hgrm/master --mirror
-repo sync -j4
