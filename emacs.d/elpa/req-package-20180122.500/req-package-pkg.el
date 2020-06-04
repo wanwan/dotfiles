816aa2b2d28dfb4939cae92a8aaad43bdc31aec2
WAKABAYASHI, Ayumu <sakuraduka@gmail.com>
2020-06-04 12:22:46 +0900

    sync_dotfiles.sh @ lela (2020-06-04_12:22:45)

---
+++ emacs.d/elpa/req-package-20180122.500/req-package-pkg.el
@@ -0,0 +1,15 @@
+(define-package "req-package" "20180122.500" "A use-package wrapper for package runtime dependencies management"
+  '((use-package "1.0")
+    (dash "2.7.0")
+    (log4e "0.2.0")
+    (ht "0"))
+  :commit "0c0ac7451149dac6bfda2adfe959d1df1c273de6" :keywords
+  '("dotemacs" "startup" "speed" "config" "package")
+  :authors
+  '(("Edward Knyshov" . "edvorg@gmail.com"))
+  :maintainer
+  '("Edward Knyshov" . "edvorg@gmail.com")
+  :url "https://github.com/edvorg/req-package")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
