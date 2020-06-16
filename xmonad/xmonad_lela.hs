81c1853a34e9a8c388654175b55f8998db2c018b
WAKABAYASHI, Ayumu <sakuraduka@gmail.com>
2020-06-16 23:06:19 +0900

    sync_dotfiles.sh @ lela (2020-06-16_23:06:18)

--- xmonad/xmonad_lela.hs
+++ xmonad/xmonad_lela.hs
@@ -40,14 +40,14 @@ myBorderWidth = 3
 
 -- define workspace
 myWorkspaces = [ "shell"
-	       ,"editor"
-	       ,"browser"
-	       ,"slack"
-	       ,"vm"
-	       ,"chat"
-	       ,"media"
-	       ,"game"
-	       ,"system" ]
+               ,"editor"
+               ,"browser"
+               ,"slack"
+               ,"vm"
+               ,"chat"
+               ,"media"
+               ,"game"
+               ,"system" ]
 
 --myStartupHook :: X()
 --myStartupHook = do
@@ -56,23 +56,25 @@ myWorkspaces = [ "shell"
 --  spawnOn "editor" "emacs"
 --  spawnOn "browser" "google-chrome-stable"
 --  --spawnOn "browser" "google-chrome-stable --force-device-scale-factor=2"
---  --spawnOn "idea" "intellij-idea-ultimate-edition"  
+--  --spawnOn "idea" "intellij-idea-ultimate-edition"
 
 myManageHookFloat = composeAll
-    [ className =? "MotionPro"             --> doFloat
+    [ className =? "MotionPro"                 --> doFloat,
+      className =? "microsoft teams - preview" --> doFloat,
+      className =? "Microsoft Teams - Preview" --> doFloat
     ]
 
 tall = Tall 1 (3/100) (1/2)
 
---main = 	xmonad $ gnomeConfig
-main = 	xmonad defaultConfig
-	{ 
-		borderWidth = 2,
-		normalBorderColor  = "#99ccff",
-		focusedBorderColor = "#ff0000", -- blue
-		modMask = myModMask,
+--main =        xmonad $ gnomeConfig
+main =          xmonad defaultConfig
+        { 
+                borderWidth = 2,
+                normalBorderColor  = "#99ccff",
+                focusedBorderColor = "#ff0000", -- blue
+                modMask = myModMask,
 --                startupHook      = myStartupHook <+> docksStartupHook,
-		manageHook	 = myManageHookFloat <+> manageDocks,
-		terminal = myTerminal
+                manageHook       = myManageHookFloat <+> manageDocks,
+                terminal = myTerminal
 
         }
