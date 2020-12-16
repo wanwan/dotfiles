3584c2f4be833a6b807d0b666de81fb9890932e8
WAKABAYASHI, Ayumu <sakuraduka@gmail.com>
2020-12-16 10:22:33 +0900

    sync_dotfiles.sh @ lela (2020-12-16_10:22:33)

---
+++ emacs.d/elpa/kmb-0.1/kmb-autoloads.el
@@ -0,0 +1,15 @@
+;;; kmb-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("kmb.el") (24537 21666 263126 126000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; kmb-autoloads.el ends here
